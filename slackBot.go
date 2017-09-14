04340d2b11db62535d0386d907dd68c644c0352b
watariMacBookPro <watariMac@inmac.local>
2017-09-14 16:12:41 +0900

    TODOリスト作成の雛形みたいなの作りました add todo ~ とうつとTODOリストに追加しましたと返します(実際は追加してない)

--- slackBot.go
+++ slackBot.go
@@ -1,37 +1,50 @@
 package main
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
+	"strings"
 
-    "github.com/nlopes/slack"
+	"github.com/nlopes/slack"
 )
 
 func run(api *slack.Client) int {
-    rtm := api.NewRTM()
-    go rtm.ManageConnection()
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
 
-    for {
-        select {
-        case msg := <-rtm.IncomingEvents:
-            switch ev := msg.Data.(type) {
-            case *slack.HelloEvent:
-                log.Print("Hello Event")
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			switch ev := msg.Data.(type) {
+			case *slack.HelloEvent:
+				log.Print("Hello Event")
 
-            case *slack.MessageEvent:
-                log.Printf("Message: %v\n", ev)
-                rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", ev.Channel))
+			case *slack.MessageEvent:
+				
+				//メッセージが"add todo"始まりならTODOリストに追加
+				if strings.HasPrefix(ev.Text, "add todo ") {
+					todoStr := strings.Replace(ev.Text, "add todo ", "", 1)
+					//TODO useridのファイル名にTODOを追加
+					rtm.SendMessage(rtm.NewOutgoingMessage("「"+todoStr+"」をTODOに追加しました。", ev.Channel))
+				}
 
-            case *slack.InvalidAuthEvent:
-                log.Print("Invalid credentials")
-                return 1
+				//メッセージが"ls todo"ならTODO一覧を表示
+				if ev.Text == "ls todo" {
+					//TODO useridのファイル名からTODOを取得
+					//TODO 一覧をメッセージで表示
+				}
+				log.Printf("Message: %+v\n", ev)
+				
+			case *slack.InvalidAuthEvent:
+				log.Print("Invalid credentials")
+				return 1
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
 
 func main() {
-    api := slack.New(getApiToken())
-    os.Exit(run(api))
+	api := slack.New(getApiToken())
+	os.Exit(run(api))
 }
