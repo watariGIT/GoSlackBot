c5e19b6f978d8b0f616eca0d16f6a37a46c1cadc
watariMacBookPro <watariMac@inmac.local>
2017-09-15 14:40:56 +0900

    勤怠管理のファイル書き出し仮実装

--- timeManagement.go
+++ timeManagement.go
@@ -1,24 +1,54 @@
 package main
 
 import (
-	"time"
-
 	"github.com/nlopes/slack"
+	"time"
+	"log"
+	"os"
+	"fmt"
+	"bufio"
+	"strings"
 )
 
 func timeManagement(ev *slack.MessageEvent, rtm *slack.RTM) {
 	//出勤処理
 	if ev.Text == "出勤" {
-		startTime := time.Now()
-		//TODO 出勤処理
-		rtm.SendMessage(rtm.NewOutgoingMessage(startTime.Format("2006/01/02 Mon 15:04:05")+"に出勤を確認しました", ev.Channel))
+		inTime := time.Now()
+
+		// 出勤書き込み
+		file, err := os.OpenFile("files/timestamp_"+ev.User+".txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		//すでに出勤していないかの確認
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			scanStr:=scanner.Text()
+			if strings.HasPrefix(scanStr,inTime.Format("2006/01/02")) && strings.HasSuffix(scanStr,"出勤"){
+				rtm.SendMessage(rtm.NewOutgoingMessage("すでに出勤しています", ev.Channel))		
+				return
+			}
+		}
+		
+		fmt.Fprintln(file,inTime.Format("2006/01/02,15:04:05")+",出勤" )
+		rtm.SendMessage(rtm.NewOutgoingMessage(inTime.Format("2006/01/02 Mon 15:04:05")+"に出勤を確認しました", ev.Channel))
 	}
 
 	//退勤処理
 	if ev.Text == "退勤" {
-		endTime := time.Now()
-		//TODO 退勤処理
-		rtm.SendMessage(rtm.NewOutgoingMessage(endTime.Format("2006/01/02 Mon 15:04:05")+"に退勤を確認しました", ev.Channel))
+		outTime := time.Now()
+
+		//退勤書き込み
+		file, err := os.OpenFile("files/timestamp_"+ev.User+".txt", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		fmt.Fprintln(file,outTime.Format("2006/01/02,15:04:05")+",退勤" )
+		
+		rtm.SendMessage(rtm.NewOutgoingMessage(outTime.Format("2006/01/02 Mon 15:04:05")+"に退勤を確認しました", ev.Channel))
 	}
 
 }
